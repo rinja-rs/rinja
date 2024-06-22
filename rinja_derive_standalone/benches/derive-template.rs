f54a985d41eeb530d5b5d508b297e12bc14790e4
René Kijewski <rene.kijewski@fu-berlin.de>
2024-06-22 16:58:18 +0200

    derive: use macro to dedup code

--- rinja_derive_standalone/benches/derive-template.rs
+++ rinja_derive_standalone/benches/derive-template.rs
@@ -25,56 +25,40 @@ fn hello_world(c: &mut Criterion) {
 }
 
 fn librustdoc(c: &mut Criterion) {
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // item_info.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
+    macro_rules! benches {
+        ($($name:expr => $struct:item)*) => { $({
+            const SOURCE: &str =
+                include_str!(concat!("../../rinja_parser/benches/librustdoc/", $name));
 
-    let source = include_str!("../../rinja_parser/benches/librustdoc/item_info.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+            let ts = quote! {
+                #[derive(Template)]
+                #[template(source = #SOURCE, ext = "html")]
+                $struct
+            };
+            c.bench_function($name, |b| {
+                b.iter_batched(
+                    || ts.clone(),
+                    |ts| rinja_derive_standalone::derive_template2(ts),
+                    BatchSize::LargeInput,
+                )
+            });
+        })* };
+    }
+
+    benches! {
+        "item_info.html" =>
         struct ItemInfo {
             items: Vec<ShortItemInfo>,
         }
-    };
-    c.bench_function("item_info.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // item_union.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/item_union.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "item_union.html" =>
         struct ItemUnion<'a, 'cx> {
             cx: RefCell<&'a mut Context<'cx>>,
             it: &'a clean::Item,
             s: &'a clean::Union,
         }
-    };
-    c.bench_function("item_union.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // page.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/page.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "page.html" =>
         struct PageLayout<'a> {
             static_root_path: String,
             page: &'a Page<'a>,
@@ -90,23 +74,8 @@ fn librustdoc(c: &mut Criterion) {
             display_krate_version_number: &'a str,
             display_krate_version_extra: &'a str,
         }
-    };
-    c.bench_function("page.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // print_item.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/print_item.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "print_item.html" =>
         struct ItemVars<'a> {
             typ: &'a str,
             name: &'a str,
@@ -115,23 +84,8 @@ fn librustdoc(c: &mut Criterion) {
             stability_since_raw: &'a str,
             src_href: Option<&'a str>,
         }
-    };
-    c.bench_function("print_item.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // short_item_info.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/short_item_info.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "short_item_info.html" =>
         enum ShortItemInfo {
             /// A message describing the deprecation of this item
             Deprecation {
@@ -147,23 +101,8 @@ fn librustdoc(c: &mut Criterion) {
                 message: String,
             },
         }
-    };
-    c.bench_function("sidebar.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // sidebar.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/sidebar.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "sidebar.html" =>
         pub(super) struct Sidebar<'a> {
             pub(super) title_prefix: &'static str,
             pub(super) title: &'a str,
@@ -172,78 +111,26 @@ fn librustdoc(c: &mut Criterion) {
             pub(super) blocks: Vec<LinkBlock<'a>>,
             pub(super) path: String,
         }
-    };
-    c.bench_function("sidebar.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // source.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/source.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "source.html" =>
         struct Source<Code: std::fmt::Display> {
             embedded: bool,
             needs_expansion: bool,
             lines: RangeInclusive<usize>,
             code_html: Code,
         }
-    };
-    c.bench_function("source.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // type_layout.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/type_layout.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "type_layout.html" =>
         struct TypeLayout<'cx> {
             variants: Vec<(Symbol, TypeLayoutSize)>,
             type_layout_size: Result<TypeLayoutSize, &'cx LayoutError<'cx>>,
         }
-    };
-    c.bench_function("type_layout.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
 
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-    // type_layout_size.html
-    // ///////////////////////////////////////////////////////////////////////////////////////////
-
-    let source = include_str!("../../rinja_parser/benches/librustdoc/type_layout_size.html");
-    let ts = quote! {
-        #[derive(Template)]
-        #[template(source = #source, ext = "html")]
+        "type_layout_size.html" =>
         struct TypeLayoutSize {
             is_unsized: bool,
             is_uninhabited: bool,
             size: u64,
         }
-    };
-    c.bench_function("type_layout_size.html", |b| {
-        b.iter_batched(
-            || ts.clone(),
-            |ts| rinja_derive_standalone::derive_template2(ts),
-            BatchSize::LargeInput,
-        )
-    });
+    }
 }
