c860a22e854089ffb047b621860dc452c9eb8f62
Guillaume Gomez <guillaume1.gomez@gmail.com>
2024-07-30 22:00:25 +0200

    Add test for rest pattern

--- testing/tests/rest_pattern.rs
+++ testing/tests/rest_pattern.rs
@@ -1,78 +1,26 @@
 use rinja::Template;
 
-#[test]
-fn a() {
-    #[derive(Template)]
-    #[template(source = "{% if let (a, ..) = abc  %}-{{a}}-{% endif %}", ext = "txt")]
-    struct Tmpl {
-        abc: (u32, u32, u32),
-    }
-
-    assert_eq!(Tmpl { abc: (1, 2, 3) }.to_string(), "-1-");
-}
-
-#[test]
-fn ab() {
-    #[derive(Template)]
-    #[template(
-        source = "{% if let (a, b, ..) = abc  %}-{{a}}{{b}}-{% endif %}",
-        ext = "txt"
-    )]
-    struct Tmpl {
-        abc: (u32, u32, u32),
-    }
-
-    assert_eq!(Tmpl { abc: (1, 2, 3) }.to_string(), "-12-");
-}
+#[derive(Template)]
+#[template(
+    source = r#"
+{%- if let [1, 2, who @ .., 4] = [1, 2, 3, 4] -%}
+111> {{"{:?}"|format(who)}}
+{%- endif -%}
+{%- if let [who @ .., 4] = [1, 2, 3, 4] -%}
+222> {{"{:?}"|format(who)}}
+{%- endif -%}
+{%- if let [1, who @ ..] = [1, 2, 3, 4] -%}
+333> {{"{:?}"|format(who)}}
+{%- endif -%}
+"#,
+    ext = "txt"
+)]
+struct Rest;
 
 #[test]
-fn abc() {
-    #[derive(Template)]
-    #[template(
-        source = "{% if let (a, b, c, ..) = abc  %}-{{a}}{{b}}{{c}}-{% endif %}",
-        ext = "txt"
-    )]
-    struct Tmpl1 {
-        abc: (u32, u32, u32),
-    }
-
-    assert_eq!(Tmpl1 { abc: (1, 2, 3) }.to_string(), "-123-");
-
-    assert_eq!(Tmpl2 { abc: (1, 2, 3) }.to_string(), "-123-");
-
-    #[derive(Template)]
-    #[template(
-        source = "{% if let (a, b, c, ..) = abc  %}-{{a}}{{b}}{{c}}-{% endif %}",
-        ext = "txt"
-    )]
-    struct Tmpl2 {
-        abc: (u32, u32, u32),
-    }
-
-    assert_eq!(Tmpl2 { abc: (1, 2, 3) }.to_string(), "-123-");
-}
-
-#[test]
-fn bc() {
-    #[derive(Template)]
-    #[template(
-        source = "{% if let (.., b, c) = abc  %}-{{b}}{{c}}-{% endif %}",
-        ext = "txt"
-    )]
-    struct Tmpl {
-        abc: (u32, u32, u32),
-    }
-
-    assert_eq!(Tmpl { abc: (1, 2, 3) }.to_string(), "-23-");
-}
-
-#[test]
-fn c() {
-    #[derive(Template)]
-    #[template(source = "{% if let (.., c) = abc  %}-{{c}}-{% endif %}", ext = "txt")]
-    struct Tmpl {
-        abc: (u32, u32, u32),
-    }
-
-    assert_eq!(Tmpl { abc: (1, 2, 3) }.to_string(), "-3-");
+fn test_rest() {
+    assert_eq!(
+        Rest.render().unwrap(),
+        "111> [3]222> [1, 2, 3]333> [2, 3, 4]"
+    );
 }
