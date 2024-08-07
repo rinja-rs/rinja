0628afaf5efd861dcb4967eae3e9f1b3e5e7735d
René Kijewski <rene.kijewski@fu-berlin.de>
2024-08-07 20:28:33 +0200

    parser: add wrong node terminator test

---
+++ testing/tests/ui/wrong-end.rs
@@ -0,0 +1,27 @@
+use rinja::Template;
+
+#[derive(Template)]
+#[template(source = "{% for _ in 1..=10 %}{% end %}", ext = "txt")]
+struct For;
+
+#[derive(Template)]
+#[template(source = "{% macro test() %}{% end %}", ext = "txt")]
+struct Macro;
+
+#[derive(Template)]
+#[template(source = "{% filter upper %}{% end %}", ext = "txt")]
+struct Filter;
+
+#[derive(Template)]
+#[template(source = "{% match () %}{% when () %}{% end %}", ext = "txt")]
+struct Match;
+
+#[derive(Template)]
+#[template(source = "{% block body %}{% end %}", ext = "txt")]
+struct Block;
+
+#[derive(Template)]
+#[template(source = "{% if true %}{% end %}", ext = "txt")]
+struct If;
+
+fn main() {}
