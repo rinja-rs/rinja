66dac1df064aab1118c39ec3504d4d0725726e80
René Kijewski <rene.kijewski@fu-berlin.de>
2024-11-26 07:23:37 +0100

    Add failing test from issue 272
    Reported by Ciprian Dorin Craciun ([@cipriancraciun]) in issue [#272].
    
    [#272]: <https://github.com/rinja-rs/rinja/issues/272>
    [@cipriancraciun]: <https://github.com/cipriancraciun>

--- testing/tests/include.rs
+++ testing/tests/include.rs
@@ -53,3 +53,35 @@ fn test_include_macro() {
 
     assert_eq!(template.render().unwrap(), "Hello, Alice!\nHowdy, Bob!");
 }
+
+// FIXME: <https://github.com/rinja-rs/rinja/issues/272>
+#[test]
+#[should_panic(expected = r#"assertion `left == right` failed
+  left: "block_in_base: from extended!\nblock_in_partial: from partial!\n"
+ right: "block_in_base: from extended!\nblock_in_partial: from extended!\n"#)]
+fn block_in_include() {
+    #[derive(Template)]
+    #[template(path = "block_in_include_extended.html")]
+    struct TmplExtended;
+
+    #[derive(Template)]
+    #[template(path = "block_in_include_base.html", block = "block_in_base")]
+    struct TmplBlockInBase;
+
+    #[derive(Template)]
+    #[template(path = "block_in_include_partial.html", block = "block_in_partial")]
+    struct TmplBlockInPartial;
+
+    assert_eq!(
+        TmplExtended.render().unwrap(),
+        "block_in_base: from extended!\nblock_in_partial: from extended!\n"
+    );
+    assert_eq!(
+        TmplBlockInBase.render().unwrap(),
+        "block_in_base: from extended!\n"
+    );
+    assert_eq!(
+        TmplBlockInPartial.render().unwrap(),
+        "block_in_partial: from extended!\n"
+    );
+}
